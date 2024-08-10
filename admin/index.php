34eac4d82f948877b92b4125436f6027f55f66d4
Dolon Roy <dolonr718@gmail.com>
2024-08-10 11:20:55 +0600

    admin stats added commit

--- admin/index.php
+++ admin/index.php
@@ -13,9 +13,26 @@
         ?>
     </header>
 
-    <main class="container">
+    <main class="container admin-panel-wrapper">
         <h1 class="title">Welcome to the Admin Panel</h1>
         <p class="description">This is the main dashboard for the admin area.</p>
+
+        <section class="stat-container">
+            <div class="stat stat-customer">
+                <h2>Total Customers</h2>
+                <p>1234</p>
+            </div>
+
+            <div class="stat stat-revenue">
+                <h2>Total Revenue</h2>
+                <p>1234</p>
+            </div>
+
+            <div class="stat stat-products">
+                <h2>Total Products</h2>
+                <p>1234</p>
+            </div>
+        </section>
     </main>
 
     <?php include("./partials/footer.php") ?>
