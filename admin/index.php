d6ec6daa3cff4ded9fe987ed3635958ca3b8b997
Dolon Roy <dolonr718@gmail.com>
2024-08-10 10:49:38 +0600

    footer added on the admin panel commit

--- admin/index.php
+++ admin/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/admin.css   ">
+    <link rel="stylesheet" href="../css/admin.css">
     <title>Home - Admin Panel</title>
 </head>
 <body>
@@ -17,5 +17,7 @@
         <h1 class="title">Welcome to the Admin Panel</h1>
         <p class="description">This is the main dashboard for the admin area.</p>
     </main>
+
+    <?php include("./partials/footer.php") ?>
 </body>
 </html>
---
+++ admin/partials/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/admin.css">
+    <title>Footer - Admin Panel</title>
+</head>
+
+<body>
+    <footer class="footer">
+        <div class="container">
+            <p class="footer-content">All &copy; Right Deserve <a target="_blank" href="https://github.com/DeveloperDolon">@DeveloperDolon</a> </p>
+        </div>
+    </footer>
+</body>
+
+</html>
